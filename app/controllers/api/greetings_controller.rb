8834fdfcb6c6954e9d955f50528679ae3c30ee17
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-01 13:52:20 +0300

    Add greetings api

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,6 @@
+class Api::GreetingsController < ApplicationController
+  def random
+    random_greeting = Greeting.order("RANDOM()").first
+    render json: { message: random_greeting&.message || "No greetings available" }
+  end
+end
